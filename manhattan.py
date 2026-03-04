93b9f61eb2e1fac2a807fd80cf1f486b8c706368
Buruian Miruna <miruna.buruian@student.usv.ro>
2026-03-04 22:05:42 +0200

    Add files via upload

--- manhattan.py
+++ manhattan.py
@@ -1,17 +1,17 @@
+#import functia cityblock din biblio scipy
 from scipy.spatial.distance import cityblock
 
 def manhattan_manual(x, y):
-    """
-    Calculează distanța Manhattan manual.
-    """
+    #fct care calc distanta manhattan manual intre doi vect x, y
+    #initializ var dist cu 0, aici vom aduna diferentele absolute
     dist = 0
-    for i in range(len(x)):
-        dist += abs(x[i] - y[i])
+    #parcurg fiecare poz din vector
+    for i in range(len(x)):#nr de elem din vect(len(x))
+        dist += abs(x[i] - y[i])#calc dif dintre elem coresp si fol abs() ca sa obtinem val abs
     return dist
 
 
 def manhattan_scipy(x, y):
-    """
-    Calculează distanța Manhattan folosind scipy.
-    """
+    #fct care calc dist manhattan fol cityblock din scipy
+    #apelam fct din biblioteca si returnam rezultatul
     return cityblock(x, y)
